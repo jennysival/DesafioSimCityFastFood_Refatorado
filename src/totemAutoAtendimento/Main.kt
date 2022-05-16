c1ea5456d2c01a955779a86a61f45fd3524e7074
jennysival <jeniffersival@gmail.com>
2022-05-16 16:26:05 -0300

    Adicionar boas vindas

--- src/totemAutoAtendimento/Main.kt
+++ src/totemAutoAtendimento/Main.kt
@@ -1,8 +1,12 @@
 package totemAutoAtendimento
 
 import totemAutoAtendimento.menus.Menu
+import totemAutoAtendimento.utilitarias.DIVISOR
 
 fun main() {
+    println(DIVISOR)
+    println("                  SIM CITY FASTFOOD :)                  ")
+
     val menu = Menu()
     menu.menuInicial()
 }
